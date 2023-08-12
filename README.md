741f2cc50b22bf3fd43f8390673a4283d8772c88
Annie <overheads165@gmail.com>
2023-08-12 16:02:44 +0200

    init

---
+++ README.md
@@ -0,0 +1,21 @@
+# ls example
+
+[![PyPI - Version](https://img.shields.io/pypi/v/ls-example.svg)](https://pypi.org/project/ls-example)
+[![PyPI - Python Version](https://img.shields.io/pypi/pyversions/ls-example.svg)](https://pypi.org/project/ls-example)
+
+-----
+
+**Table of Contents**
+
+- [Installation](#installation)
+- [License](#license)
+
+## Installation
+
+```console
+pip install ls-example
+```
+
+## License
+
+`ls-example` is distributed under the terms of the [MIT](https://spdx.org/licenses/MIT.html) license.
---
+++ tests/test_ls_pages.py
@@ -0,0 +1,78 @@
+from playwright.sync_api import Page, expect
+import re
+import pytest
+from loguru import logger
+
+BASE_URL = "https://www.lightspeedhq.com/"
+
+
+@pytest.fixture(scope="function", autouse=True)
+def to_intro_page(page: Page):
+    logger.info(f"Going to the base URL {BASE_URL}")
+    # Go to the starting url before each test.
+    page.goto(BASE_URL)
+    yield
+
+
+def test_page_title(page: Page):
+    expect(page).to_have_title(re.compile("Lightspeed"))
+
+
+def test_watch_demo(page: Page):
+    # close the popup
+    page.get_by_role("button", name="Close").click()
+    # click the watch demo link
+    page.get_by_role("link", name="Watch a demo").first.click()
+    # check that retail box is visible
+    test_values = ["Retail", "Restaurant", "Golf"]
+    for test_value in test_values:
+        logger.info(f"Checking for {test_value}")
+        test_box = page.locator("form").get_by_role("button", name=test_value)
+        expect(test_box).not_to_be_empty()
+        expect(test_box).to_be_visible()
+        expect(test_box).to_have_text(test_value, use_inner_text=True)
+
+
+def test_login_page(page: Page):
+    page.get_by_role("button", name="Close").click()
+    page.get_by_role("menuitem", name="Login").click()
+
+    page.get_by_text("Retail POS (X-Series) formerly Vend").click()
+    page.locator("p").filter(has_text="Restaurant POS (L-Series)").click()
+    page.locator("p").filter(has_text="eCommerce (E-Series)").click()
+
+    test_values = ["Retail POS (X-Series) formerly Vend", "Restaurant POS (L-Series)", "eCommerce (E-Series)"]
+    for test_value in test_values:
+        logger.info(f"Checking for {test_value}")
+        test_box = page.locator("p").filter(has_text=test_value)
+        expect(test_box).not_to_be_empty()
+        expect(test_box).to_be_visible()
+        expect(test_box).to_have_text(test_value, use_inner_text=True)
+
+
+@pytest.mark.parametrize(["email", "case"], [("test@example.com", "positive"), ("testNotAnEmail", "negative")])
+def test_email_input(page: Page, email: str, case: str):
+    page.goto("https://www.lightspeedhq.com/partners/partner-application/")
+    email_form = page.query_selector("#leadform-1_email")
+    email_form.fill(email)
+    page.mouse.click(1, 1) # click away from the form to trigger the input validation
+    error_message = page.get_by_text("Please enter a valid email address")
+
+    if case == "negative":
+        expect(error_message).to_be_visible()
+    else:
+        expect(error_message).to_be_hidden()
+
+@pytest.mark.parametrize(["phone", "case"], [("874294834", "positive"), ("testNotAPhone", "negative")])
+def test_phone_input(page: Page, phone: str, case: str):
+    page.goto("https://www.lightspeedhq.com/partners/partner-application/")
+    phone_form = page.query_selector("#leadform-1_phone")
+    phone_form.fill(phone)
+    page.mouse.click(1, 1)
+    error_message = page.get_by_text("Please enter a valid phone number")
+
+    if case == "negative":
+        expect(error_message).to_be_visible()
+    else:
+        expect(error_message).to_be_hidden()
+        
